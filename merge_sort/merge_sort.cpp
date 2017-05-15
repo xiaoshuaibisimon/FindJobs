f72c65dc2a4461b5983b96b9298f89df057ef3b2
xiaoshuaibisimon <13370320852@163.com>
2017-05-15 19:01:05 +0800

    添加归并排序注释

--- merge_sort/merge_sort.cpp
+++ merge_sort/merge_sort.cpp
@@ -17,13 +17,15 @@ merge(void *data,int esize,
     jpos = mid+1;
     mpos = 0;
 
+    //归并的时候需要多一倍的空间
     if((m = (char*)malloc(esize * (right - left +1))) = NULL)
         return -1;
 
+    //只要两个区间的任何一个还有元素需要放入归并序列里就继续归并过程
     while(ipos <= mid || jpos <= right)
     {
         if(ipos > mid)
-        {
+        {//左区间已经全部放入归并序列--右边还有元素剩余
             while(jpos <= right)
             {
                 memcpy(&m[mpos*esize],&a[jpos*esize],esize);
@@ -33,7 +35,7 @@ merge(void *data,int esize,
             continue;
         }
         else if(jpos > right)
-        {
+        {//右区间已经全部放入归并序列--左边还有元素剩余
             while(ipos <= mid)
             {
                 memcpy(&m[mpos*esize],&a[ipos*esize],esize);
@@ -43,23 +45,26 @@ merge(void *data,int esize,
             continue;
         }
 
-        if(compare(&a[ipos*esize],&a[jpos*esize]) < 0)
+        //两个区间都还需要放入归并队列
+        if(compare(&a[ipos*esize],&a[jpos*esize]) < 0)//左区间元素小
         {
             memcpy(&m[mpos*esize],&a[ipos*esize],esize);
-            ipos++;
+            ipos++;//哪个区间放入归并序列--记录该区间变化位置即可
             mpos++;
         }
-        else
+        else//右区间元素小
         {
             memcpy(&m[mpos*esize],&a[jpos*esize],esize);
             jpos++;
             mpos++;
         }
 
-    }
+    }//两个区间所有元素都放入了归并序列
 
+    //将归并序列复制到原始序列对应的位置
     memcpy(&a[left*esize],m,esize*(right - left + 1));
 
+    //回收内存
     free(m);
 
     return 0;
@@ -73,19 +78,19 @@ mgsort(void *data,int size,int esize,
 {
     int mid;
 
-    if(left < right)
+    if(left < right)//左边下标大于等于右边小标--递归结束--归并排序完成
     {
-        mid = (int)((right - left + 1)/2);
+        mid = (int)((right - left + 1)/2);//求出中间位置
 
-        if(mgsort(data,size,esize,left,mid,compare) < 0)
+        if(mgsort(data,size,esize,left,mid,compare) < 0)//归并排序左区间
             return -1;
 
-        if(mgsort(data,size,esize,mid+1,right,compare) < 0)
+        if(mgsort(data,size,esize,mid+1,right,compare) < 0)//归并右区间
             return -1;
 
-        if(merge(data,esize,left,mid,right,compare) < 0)
+        if(merge(data,esize,left,mid,right,compare) < 0)//对有序子序列进行归并
             return -1;
-    }
+    }//data指向已经排好序的集合
 
     return 0;
 }
