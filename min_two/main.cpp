8f0d2b9db6f706c33fd0d5d9c7893bc53137c0e4
xiaoshuaibisimon <13370320852@163.com>
2017-05-17 17:36:14 +0800

    添加了最小二乘法

---
+++ min_two/main.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+void
+lsqe(const double *x,const double *y,int n,double *b1,double *b0)
+{
+    double sumx,sumy,sumx2,sumxy;
+
+    int i;
+
+    sumx = 0.0;
+    sumy = 0.0;
+
+    sumxy = 0.0;
+    sumx2 = 0.0;
+
+    for(i = 0;i < n;i++)
+    {//遍历x和y，求出累加和
+        sumx += x[i];
+        sumy += y[i];
+
+        sumx2 += pow(x[i],2.0);
+        sumxy += (x[i]*y[i]);
+    }
+
+    *b1 = (sumxy - (sumx*sumy)/((double)n))/(sumx2-(sumx*sumx)/((double)n));//重要公式
+    *b0 = (sumy - (*b1)*sumx)/((double)n);//重要公式
+
+    return ;
+}
+
+
+int main()
+{
+    return 0;
+}
+
+
+
